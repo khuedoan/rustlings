0b4d1fdbe06d212200715dec1f1113b9457e8398
Khue Doan <mail@khuedoan.com>
2023-04-03 13:45:42 +0700

    fix: update convertions exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let info: Vec<&str> = s.split(",").collect();
+            let name = info[0].to_string();
+
+            if name.len() == 0 || info.len() != 2 {
+                Person::default()
+            } else {
+                if let Ok(age) = info[1].parse::<usize>() {
+                    Person {
+                        name,
+                        age,
+                    }
+                } else {
+                    Person::default()
+                }
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err(ParsePersonError::Empty)
+        } else {
+            let info: Vec<&str> = s.split(",").collect();
+            let name = info[0].to_string();
+
+            if name.len() == 0 {
+                Err(ParsePersonError::NoName)
+            } else if info.len() != 2 {
+                Err(ParsePersonError::BadLen)
+            } else {
+                match info[1].parse::<usize>() {
+                    Ok(age) => Ok(Person {
+                        name,
+                        age,
+                    }),
+                    Err(err) => Err(ParsePersonError::ParseInt(err))
+                }
+            }
+        }
     }
 }
 
